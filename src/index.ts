71501fe28fbfd8c91d0fc838e42f86c073614780
Vivek Panchal <panchal.vivek315@gmail.com>
2024-06-24 17:57:00 +0530

    is_priority and is_favourite set on task module and list order descending by is_priority and updated_by

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,17 @@ import "reflect-metadata";
 import express from "express";
 import dotenv from "dotenv";
 import { AppDataSource } from "./config/ormconfig";
-import userRoutes from "./routes/userRoutes";
-import projectRoutes from "./routes/projectRoutes";
-import labelRoutes from "./routes/labelRoutes";
-import taskRoutes from "./routes/taskRoutes";
-import notificationRoutes from "./routes/notificationRoutes";
-import commentRoutes from "./routes/commentRoutes";
 import user from "./routes/auth";
 import { errorHandler } from "./middleware/errorHandler";
+import router from "./routes"
 
 dotenv.config();
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
 
-app.use("/api/users", userRoutes);
+app.use("/api", router);
 app.use("/api/users", user);
-app.use("/api/projects", projectRoutes);
-app.use("/api/labels", labelRoutes);
-app.use("/api/tasks", taskRoutes)
-app.use("/api/notifications", notificationRoutes)
-app.use("/api/comments", commentRoutes)
-
 app.use(errorHandler);
 
 AppDataSource.initialize()
